5ae9b7a9497c017546afac4a46b31d8749676c2c
Emon <emon50666@gmail.com>
2024-09-23 11:15:44 +0600

    modify slider

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -15,43 +15,43 @@ import 'swiper/css/pagination';
 const Slider = () => {
     return (
         <div>
-        <Swiper
-            slidesPerView={1}
-            spaceBetween={30}
-            loop={true}
-            pagination={{
-                clickable: true,
-            }}
-            navigation={true}
-            modules={[Pagination, Navigation, Autoplay]}
-            className="mySwiper"
-        >
-            <SwiperSlide>
-                <div className="mx-auto xl:w-full">
-                    <img
-                        src="https://i.ibb.co.com/x2hJQbJ/2.png"
-                        layout="responsive"
-                        alt="Slide 1"
-                        priority
-                        
-                    />
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-            <div className="mx-auto xl:w-full">
-                    <img
-                        src="https://i.ibb.co.com/wdNrFS8/1.png"
-                        layout="responsive"
-                     
-                        alt="Slide 2"
-                        priority
-                    />
-                </div>
-            </SwiperSlide>
-        </Swiper>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation, Autoplay]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="mx-auto   xl:w-full 2xl:w-2/3">
+                        <img
+                            src="https://i.ibb.co.com/x2hJQbJ/2.png"
+                            alt="Slide 1"
+                            className="w-full h-auto"
+                            priority
+                        />
+                    </div>
 
-     
-    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="mx-auto   xl:w-full 2xl:w-2/3">
+                        <img
+                            src="https://i.ibb.co.com/wdNrFS8/1.png"
+                            layout="responsive"
+
+                            alt="Slide 2"
+                            priority
+                        />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+
+
+        </div>
     );
 };
 
