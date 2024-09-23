a30b5fd9e98d78298b4311b5d7d0dba946008ae1
Ajharahmed3218 <nuhash3218@gmail.com>
2024-09-23 18:06:50 +0600

    Added : Category Sectiond

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import AboutSection from "@/Components/AboutSection";
 import HeroSection from "@/Components/HeroSection";
 import OfferSection from "@/Components/OfferSection";
 import Slider from "@/Components/(Slider)/Slider";
+import Category from "@/Components/(Category )/Category";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <HeroSection></HeroSection>
       <AboutSection></AboutSection>
       <OfferSection></OfferSection>
+      <Category/>
     </div>
   );
 }
