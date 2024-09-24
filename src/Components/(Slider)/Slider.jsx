b31fcf2dbc53ede0f4b3a80343ce1a461e87e6d4
Sarfaraj525 <sarfaraj525@gmail.com>
2024-09-24 17:59:00 +0600

    Banner img added_ you can take a look

--- src/Components/(Slider)/Slider.jsx
+++ src/Components/(Slider)/Slider.jsx
@@ -28,7 +28,7 @@ const Slider = () => {
                 autoplay={{ delay: 3000 }} 
                 className={styles.mySwiper} 
             >
-                <SwiperSlide>
+                {/* <SwiperSlide>
                     <div className={styles.slideWrapper}> 
                         <img className='h-full'
                             src="https://i.ibb.co/8NszP8B/sample-1.jpg"
@@ -45,7 +45,64 @@ const Slider = () => {
                             className={styles.responsiveImage}
                         />
                     </div>
-                </SwiperSlide>
+                </SwiperSlide> */}
+
+<SwiperSlide>
+    <div className={styles.slideWrapper}>
+        <img
+            src="https://i.postimg.cc/BnfBj3fs/Flag-of-Bangladesh-svg.png"
+            alt="Slide 1"
+            className={`h-full ${styles.responsiveImage}`}
+        />
+    </div>
+</SwiperSlide>
+<SwiperSlide>
+    <div className={styles.slideWrapper}>
+        <img
+            src="https://i.postimg.cc/wB0X4syT/front-view-light-box-with-stack-books.jpg"
+            alt="Slide 2"
+            className={styles.responsiveImage}
+        />
+    </div>
+</SwiperSlide>
+<SwiperSlide>
+    <div className={styles.slideWrapper}>
+        <img
+            src="https://i.postimg.cc/B6CpP0Sf/robert-anasch-Mc-X3-Xu-JRs-UM-unsplash.jpg"
+            alt="Slide 3"
+            className={styles.responsiveImage}
+        />
+    </div>
+</SwiperSlide>
+<SwiperSlide>
+    <div className={styles.slideWrapper}>
+        <img
+            src="https://i.postimg.cc/G3BfYbVs/lightbox-still-life-arrangement.jpg"
+            alt="Slide 4"
+            className={styles.responsiveImage}
+        />
+    </div>
+</SwiperSlide>
+<SwiperSlide>
+    <div className={styles.slideWrapper}>
+        <img
+            src="https://i.postimg.cc/sX39rNp6/rey-seven-nm-m-Z4-Cs2-I-unsplash.jpg"
+            alt="Slide 5"
+            className={styles.responsiveImage}
+        />
+    </div>
+</SwiperSlide>
+<SwiperSlide>
+    <div className={styles.slideWrapper}>
+        <img
+            src="https://i.postimg.cc/nrk9ry4V/2023791-George-Eliot-Quote-Don-t-judge-a-book-by-its-cover.jpg"
+            alt="Slide 6"
+            className={styles.responsiveImage}
+        />
+    </div>
+</SwiperSlide>
+
+
             </Swiper>
         </div>
     );
