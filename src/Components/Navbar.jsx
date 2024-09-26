cef024cf0d7d79e59196e1ca6569e69aba5ba99c
Mdafsarx <mdafsar99009@gmail.com>
2024-09-26 18:57:24 +0600

    contact page desgin updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,10 +11,10 @@ const Navbar = () => {
       title: "Home",
       path: "/",
     },
-    {
-      title: "About",
-      path: "/about",
-    },
+    // {
+    //   title: "About",
+    //   path: "/about",
+    // },
     {
       title: "Contact",
       path: "/contact",
