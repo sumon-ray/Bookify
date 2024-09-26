80da20919d5660ea17b066b76098426af4a4c412
Mdafsarx <mdafsar99009@gmail.com>
2024-09-27 01:28:22 +0600

    add books and my books page creted done

--- src/app/(dashboard)/dashboard/myBooks/page.jsx
+++ src/app/(dashboard)/dashboard/myBooks/page.jsx
@@ -1,6 +1,20 @@
+"use client"
+import { useQuery } from "@tanstack/react-query";
 import MyBookCard from "./MyBookCard";
+import axios from "axios";
 
 export default function MyBooks() {
+
+    // for now show only classic category data update on future
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['My books'],
+        queryFn: async () => {
+            const res = await axios(`https://bookify-server-lilac.vercel.app/books?genre=Classic`)
+            const data = await res.data
+            return data
+        }
+    })
+
     return (
         <section className="space-y-12">
 
@@ -10,7 +24,7 @@ export default function MyBooks() {
                 {/* filter function */}
                 <div>
                     <form className="max-w-sm mx-auto">
-                        <select id="countries" className="bg-gray-50  border-black border-2 text-black text-sm rounded-lg focus:ring-black focus:border-black block w-full p-2.5 ">
+                        <select id="countries" className="bg-gray-50  border-[#EFEEE9] border text-black font-bold text-sm rounded-lg focus:ring-black focus:border-black block w-full p-2.5 ">
                             <option value="US">Fiction</option>
                             <option value="CA">Classic</option>
                             <option value="FR">Modernist</option>
@@ -20,10 +34,11 @@ export default function MyBooks() {
             </div>
 
             {/* cards */}
-            <div className="max-w-7xl mx-auto grid grid-cols-2 gap-x-16 gap-y-10 pb-12">
+            <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-x-16 gap-y-10 pb-12">
 
-                <MyBookCard />
-                <MyBookCard />
+                {
+                    data?.map((book, idx) => <MyBookCard key={idx} book={book}/>)
+                }
 
             </div>
 
