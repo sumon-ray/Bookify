6cc372f325751a4ede895954d92945557653026f
Mdafsarx <mdafsar99009@gmail.com>
2024-09-26 23:25:42 +0600

    my book page added

--- src/app/(dashboard)/dashboard/myBooks/page.jsx
+++ src/app/(dashboard)/dashboard/myBooks/page.jsx
@@ -1,7 +1,22 @@
+import MyBookCard from "./MyBookCard";
+
 export default function page() {
     return (
-        <div>
-            my books 
-        </div>
+        <section className="space-y-12">
+
+            {/* my book page title */}
+            <div className="bg-[#EFEEE9] rounded-md p-6">
+                <h1 className="text-3xl font-black uppercase text-center">My books</h1>
+            </div>
+
+            {/* cards */}
+            <div className="max-w-7xl mx-auto grid grid-cols-2 gap-x-16 gap-y-10 pb-12">
+
+                <MyBookCard />
+                <MyBookCard />
+
+            </div>
+
+        </section>
     )
 }
