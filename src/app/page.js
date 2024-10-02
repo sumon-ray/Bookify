c6beb50846d1c3f0e15afa1d4cb47fb3a4355561
Ajharahmed3218 <nuhash3218@gmail.com>
2024-10-02 15:47:15 +0600

    Added  : Book Reviews)

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Slider from "@/Components/(Slider)/Slider";
 import Category from "@/Components/(Category )/Category";
 import OurCollection from "@/Components/OurCollection";
 import OurLibrary from "@/Components/OurLibrary/OurLibrary";
+import BookReviews from "@/Components/BookReview/BookReviews";
 
 // import '../Components/(Slider)/style.module.css'
 
@@ -18,7 +19,7 @@ export default function Home() {
       <Slider />
       <OurCollection/>
       <OurLibrary/>
-
+      <BookReviews/>
       {/* don't touch the commented part */}
       {/* <OfferSection></OfferSection> */}
       {/* <HeroSection></HeroSection> */}
