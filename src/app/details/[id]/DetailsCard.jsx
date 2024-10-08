94a32e72c77cebbf5bc974491e7b3a6aef333130
Mdafsarx <mdafsar99009@gmail.com>
2024-10-09 01:29:52 +0600

    edit the dashboard

--- src/app/details/[id]/DetailsCard.jsx
+++ src/app/details/[id]/DetailsCard.jsx
@@ -2,6 +2,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function DetailsCard({ genre, id }) {
 
@@ -14,25 +15,37 @@ export default function DetailsCard({ genre, id }) {
         }
     })
     const relatedBook = data?.filter(v => v._id !== id)
-    
+
 
     return (
         <div className="max-w-6xl mx-auto flex justify-center">
-            <div className="grid grid-cols-3 gap-16">
+            <div className="grid grid-cols-6 gap-16">
                 {
-                    relatedBook?.map(book => <div key={book?._id}
-                        className="max-w-xs overflow-hidden bg-white px-5 pt-5 pb-2 rounded-xl border">
-                        <Image
-                            className="object-cover w-full rounded-xl h-[400px]"
-                            src={book?.coverImage}
-                            height={1000}
-                            width={1000} />
-                        <div className="text-left ">
-                            <h1 className="font-black text-center pt-1.5 text-[#000000]">{book?.title}</h1>
+                    relatedBook?.map(book => <Link 
+                        href={`/details/${book?._id}`}
+                        key={book?._id}
+                        className="w-44 h-[250px] bg-[#EFEEE9] flex flex-col justify-center items-center rounded-lg p-4">
+                        <div className="space-y-3">
+                            <Image
+                                src={book?.coverImage}
+                                className="w-36 h-40 rounded-lg"
+                                height={150}
+                                width={200}
+                                alt={book?.title || 'Book Cover'}
+                            />
+                            <div className="pl-1 text-center text-black">
+                                <h1 className="font-bold md:uppercase" title={book?.title}>
+                                    {book?.title?.split(' ').slice(0, 2).join(' ')}
+                                </h1>
+                                <h1 className="font-normal">{book?.author?.split(' ').slice(0, 2).join(' ')}</h1>
+                            </div>
                         </div>
-                    </div>)
+                    </Link>)
                 }
             </div>
         </div>
     )
 }
+
+
+
