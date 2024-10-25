78fb93f4ac4203e354ca1cbe5f81036ed4d34a92
sumon-ray <raysumon220020@gmail.com>
2024-10-25 08:06:35 +0600

    fixed deploy

--- src/app/(dashboard)/dashboard/myBooks/Banner.jsx
+++ src/app/(dashboard)/dashboard/myBooks/Banner.jsx
@@ -41,27 +41,28 @@ const Banner = () => {
 
   return (
     <div className="relative flex items-center justify-center h-[400px] w-full" style={backgroundImageStyle}>
-      <div className="md:flex hidden absolute inset-0 bg-transparent dark:border-[#272727CC]" style={backgroundImageStyle2}>
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4">
-          <h1 className="text-lg md:text-2xl lg:text-3xl font-bold leading-tight">
-          "A Book Is a Gift You Can Open Again"
-          </h1>
-          <p className="my-4 text-sm md:text-base lg:text-lg max-w-2xl hidden md:block">
-          "A book is a gift you can open again, much like dialogue—an ancient philosophical genre and a key element in fiction and drama."
-          </p>
-          <p className="my-4 text-sm md:text-base lg:text-lg max-w-2xl md:hidden">
-          "A book is a gift you can open again, much like dialogue—an ancient philosophical genre and a key element in fiction and drama."
-          </p>
-          <div className="flex gap-4 mt-4">
-            <button className="bg-[#364957] border border-white md:px-4 py-2 rounded-md text-white text-sm md:text-base">
-              Claim Discount
-            </button>
-            {/* <button className="bg-white md:px-4 py-2 rounded-md text-[#364957] text-sm md:text-base">
-              Open Flash
-            </button> */}
-          </div>
-        </div>
-      </div>
+<div className="md:flex hidden absolute inset-0 bg-transparent dark:border-[#272727CC]" style={backgroundImageStyle2}>
+  <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4">
+    <h1 className="text-lg md:text-2xl lg:text-3xl font-bold leading-tight">
+      &quot;A Book Is a Gift You Can Open Again&quot;
+    </h1>
+    <p className="my-4 text-sm md:text-base lg:text-lg max-w-2xl hidden md:block">
+      &quot;A book is a gift you can open again, much like dialogue—an ancient philosophical genre and a key element in fiction and drama.&quot;
+    </p>
+    <p className="my-4 text-sm md:text-base lg:text-lg max-w-2xl md:hidden">
+      &quot;A book is a gift you can open again, much like dialogue—an ancient philosophical genre and a key element in fiction and drama.&quot;
+    </p>
+    <div className="flex gap-4 mt-4">
+      <button className="bg-[#364957] border border-white md:px-4 py-2 rounded-md text-white text-sm md:text-base">
+        Claim Discount
+      </button>
+      {/* <button className="bg-white md:px-4 py-2 rounded-md text-[#364957] text-sm md:text-base">
+        Open Flash
+      </button> */}
+    </div>
+  </div>
+</div>
+
 
       <div className="md:hidden absolute inset-0 " style={backgroundImageStyle3}>
         <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4">
