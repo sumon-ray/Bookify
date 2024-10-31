4c76a40b3cffa41dda21fbc37b8a6b2c5f4cc10b
naeemul-online <naeemul.online@gmail.com>
2024-10-31 06:58:51 +0300

    signin and signup page updated

--- src/Components/SocialLogin.jsx
+++ src/Components/SocialLogin.jsx
@@ -35,7 +35,7 @@ const SocialLogin = () => {
       <div>
         <button
           onClick={() => handleSocialLogin("google")}
-          className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+          className="flex items-center justify-center mt-4 bg-white text-gray-600 transition-colors duration-300 transform border rounded-lg dark:bg-[#0A0A0C] dark:text-white"
         >
           <div className="px-4 py-2">
             <svg className="w-6 h-6" viewBox="0 0 40 40">
